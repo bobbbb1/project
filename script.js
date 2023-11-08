f8dc63d0744c63ba770b29da23b9749187b5ff0d
bobbbb1 <bobbyrifki45@gmail.com>
2023-11-08 07:17:17 +0700

    edited

--- script.js
+++ script.js
@@ -55,103 +55,65 @@ floatingButton.addEventListener("click", function() {
 
   
   var subBtn = document.getElementById('sub-btn');
-  var subMenu = document.querySelector('.sub-menu');
-  var arrowIcon = document.querySelector('#sub-btn .fa-solid');
-  var isOpen = false;
-  
-  subBtn.addEventListener('click', function(e) {
-    e.preventDefault(); // Mencegah link dari navigasi
-  
-    if (isOpen) {
-      // Sembunyikan submenu dengan efek fade-out
-      subMenu.style.opacity = 0;
-      setTimeout(function() {
-        subMenu.style.display = 'none';
-      }, 100); // Waktu transisi adalah 0.3 detik (300ms)
-      // Ganti ikon panah menjadi panah ke bawah
-      arrowIcon.classList.remove('fa-caret-up');
-      arrowIcon.classList.add('fa-caret-down');
-    } else {
-      // Tampilkan submenu dengan efek fade-in
-      subMenu.style.display = 'block';
-      setTimeout(function() {
-        subMenu.style.opacity = 1;
-      }, 0); // Sebuah penundaan kecil sebelum memunculkan submenu
-      // Ganti ikon panah menjadi panah ke atas
-      arrowIcon.classList.remove('fa-caret-down');
-      arrowIcon.classList.add('fa-caret-up');
-    }
-    isOpen = !isOpen;
-  });
-  
+var subMenu = document.querySelector('.sub-menu');
+var arrowIcon = document.querySelector('#sub-btn .fa-solid');
+var isOpen = false;
 
+subBtn.addEventListener('click', function(e) {
+  e.preventDefault(); // Mencegah link dari navigasi
 
-
-
-
-
-
-  /* Contact */
-
-  /* navbar */
-
-  // Cek apakah ada status navbar yang tersimpan di localStorage
-  var isNavbarHidden = localStorage.getItem('navbarHidden') === 'true';
-
-  // Fungsi untuk menampilkan atau menyembunyikan navbar
-  function toggleNavbar() {
-    var navbar = document.getElementById("navbar");
-    
-    if (isNavbarHidden) {
-      navbar.style.top = "-200px"; // Sesuaikan dengan nilai yang sesuai
-    } else {
-      navbar.style.top = "0";
-    }
+  if (isOpen) {
+    closeSubMenu();
+  } else {
+    openSubMenu();
   }
+  isOpen = !isOpen;
+});
 
-  // Cek status navbar saat halaman dimuat
-  window.onload = function() {
-    toggleNavbar();
-  };
+// Tambahkan event listener ke elemen document untuk menutup submenu saat mengklik di luar submenu
+document.addEventListener('click', function(e) {
+  if (!subBtn.contains(e.target) && !subMenu.contains(e.target)) {
+    closeSubMenu();
+    isOpen = false;
+  }
+});
 
-  // Fungsi untuk menyimpan status navbar saat pengguna menggulir
-  window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (currentScrollPos < 50) {
-      isNavbarHidden = false;
-      localStorage.setItem('navbarHidden', 'false');
-    } else {
-      isNavbarHidden = true;
-      localStorage.setItem('navbarHidden', 'true');
-    }
-    toggleNavbar();
-  };
+function openSubMenu() {
+  subMenu.style.display = 'block';
+  setTimeout(function() {
+    subMenu.style.opacity = 1;
+  }, 0);
+  arrowIcon.classList.remove('fa-caret-down');
+  arrowIcon.classList.add('fa-caret-up');
+}
 
+function closeSubMenu() {
+  subMenu.style.opacity = 0;
+  setTimeout(function() {
+    subMenu.style.display = 'none';
+  }, 100);
+  arrowIcon.classList.remove('fa-caret-up');
+  arrowIcon.classList.add('fa-caret-down');
+}
 
-
-  /* button */
-  document.getElementById("floating-button").addEventListener("click", function() {
-    // Menggulirkan halaman ke paling bawah
-    window.scrollTo(0, document.body.scrollHeight);
-  });
-
- 
-  var floatingButton = document.getElementById("floating-button");
   
-  window.addEventListener("scroll", function() {
-    if (window.scrollY > 100) { // Ganti angka 100 sesuai kebutuhan
-      floatingButton.classList.add("hide");
-    } else {
-      floatingButton.classList.remove("hide");
-    }
-  });
 
-  floatingButton.addEventListener("click", function() {
-    window.scrollTo(0, document.body.scrollHeight);
-  });
+/*display menu */
+
+var menu = document.querySelector(".navbar-menu")
+var menuBtn = document.querySelector(".menu-btn")
+var closeBtn = document.querySelector(".close-btn")
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
 
 
 
 
 
-
+  
